e09654cef6cfcc6a62b4a50914f75b8973edacdc
sae1013 <sae1013@gmail.com>
2022-09-21 17:17:22 +0900

    스와이프 임시커밋..

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import styles from "./Home.module.scss";
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router";
+import {Swiper, SwiperSlide} from 'swiper/react';
+import 'swiper/css';
+import '../Swiper.scss';
 
 function Home() {
   const navigate = useNavigate();
@@ -10,20 +13,43 @@ function Home() {
   const goMarket = (e: React.MouseEvent) => {
     navigate(`/market`);
   };
-  
+
+  const keyVisual = [
+    {
+      imageSrc:"/collectibles.png",
+      description:'test..',
+      date:'2022-03-05'
+    }
+  ]
   return (
     <div className={styles["container"]}>
-      <section id="1" className={styles["landing-top"]}>
-        <div dangerouslySetInnerHTML={{ __html: t("common.intro") }}></div>
-        <button onClick={goMarket}>
-          <span className={styles["icon"]}>👉</span>Play Market
-        </button>
-
-        {/* <div className={styles['float-banner']}>
-          <span></span>
-        </div> */}
-      </section>
+      <div className="swiper__container">
+        <Swiper
+          slidesPerView={1}
+          threshold={70}
+        >
+          <SwiperSlide>
+            <div className="item" style={{height:'100%',backgroundImage:`url(${keyVisual[0].imageSrc})`}}>sfasdfasdfasdfasf</div>
+          </SwiperSlide>
+          
+          <SwiperSlide>
+            <div className="item" style={{height:'100%',backgroundImage:`url(${keyVisual[0].imageSrc})`}}>sfasdfasdfasdfasf</div>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
+    // <div className={styles["container"]}>
+    //   <section id="1" className={styles["landing-top"]}>
+    //     <div dangerouslySetInnerHTML={{ __html: t("common.intro") }}></div>
+    //     {/* <button onClick={goMarket}>
+    //       <span className={styles["icon"]}>👉</span>Play Market
+    //     </button> */}
+
+    //     {/* <div className={styles['float-banner']}>
+    //       <span></span>
+    //     </div> */}
+    //   </section>
+    // </div>
   );
 }
 
---
+++ src/components/home/Keyvisual.ts
@@ -0,0 +1,14 @@
+interface keyVisualItem {
+  imageSrc:string,
+  description:string,
+  date:string
+}
+export const keyVisual = [
+  {
+    imageSrc:'',
+    description:'hello',
+    data:'2022-03-05'
+
+  },
+  
+]
