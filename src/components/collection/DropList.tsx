2cc4a071638902b86ddcd7a091c64165eb7e8095
sae1013 <sae1013@gmail.com>
2022-10-13 15:20:36 +0900

    Refactor: LazyLoad

--- src/components/collection/DropList.tsx
+++ src/components/collection/DropList.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
+import React from "react";
 import styles from "./Collection.module.scss";
 import CollectionItem from "./CollectionItem";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
-function DropList({claimedNfts,initAddress}) {
-    const {t} = useTranslation()
-    return (
-        <React.Suspense fallback={<div>Load Images..</div>}>
-            <section className={`${styles.container2}`}>
-                <div className={styles.wrapper}>
-                    <h1 className={styles.section2__title}>{t('market.available')}</h1>
-                    <ul className={styles.grid}>
-                        {claimedNfts?.map((nft, index) => {
-                            return (
-                                <CollectionItem initAddress={initAddress} key={index} owner={nft.owner} imageSrc={nft.metadata.image as string} title={nft.metadata.name as string} ></CollectionItem>
-                            );
-                        })}
-                    </ul>
-                </div>
-            </section>
-        </React.Suspense>
-    );
+function DropList({ claimedNfts, initAddress }) {
+  const { t } = useTranslation();
+  return (
+    <section className={`${styles.container2}`}>
+      <div className={styles.wrapper}>
+        <h1 className={styles.section2__title}>{t("market.available")}</h1>
+        <ul className={styles.grid}>
+          {claimedNfts?.map((nft, index) => {
+            return (
+              <CollectionItem
+                initAddress={initAddress}
+                key={index}
+                owner={nft.owner}
+                imageSrc={nft.metadata.image as string}
+                title={nft.metadata.name as string}
+              ></CollectionItem>
+            );
+          })}
+        </ul>
+      </div>
+    </section>
+  );
 }
 
 export default DropList;
