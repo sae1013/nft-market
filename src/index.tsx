c21474461c736f55da7d22314007e5dafce3dcbd
sae1013 <sae1013@gmail.com>
2022-10-29 18:35:43 +0900

    path-package로 node_modules 직접수정 third-web,ether.js api defualt key수정

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "../src/i18n";
 import reportWebVitals from "./reportWebVitals";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { ChainId, ThirdwebProvider,useSDK } from "@thirdweb-dev/react";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { ThemeProvider } from "styled-components";
 import GlobalTheme from "./styles/index";
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -17,7 +18,7 @@ const queryClient = new QueryClient();
 root.render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+      <ThirdwebProvider desiredChainId={ChainId.Goerli} >
         <ThemeProvider theme={GlobalTheme}>
           <App />
         </ThemeProvider>
